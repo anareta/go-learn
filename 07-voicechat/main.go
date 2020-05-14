0c6d372fdce11841daa24f642a3cf4d52bf18a32
Az <vbvbnbnvn@gmail.com>
2020-05-14 17:16:46 +0900

    ローカルでのビデオチャット

--- 07-voicechat/main.go
+++ 07-voicechat/main.go
@@ -2,83 +2,47 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/jcuga/golongpoll"
+	"github.com/satori/go.uuid"
+	"golang.org/x/net/websocket"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
-type messages struct {
-	Message []message
-}
-
-type message struct {
-	Body string
-	Time string
-}
-
-// ロングポーリング
-var manager, _ = golongpoll.StartLongpoll(golongpoll.Options{})
-
-var messageDatabase = messages{Message: []message{}}
+const (
+	TYPE_CONNECTED = "connected"
+	TYPE_ENTER     = "enter"
+	TYPE_LEAVE     = "leave"
+)
 
 func main() {
-	fmt.Println("The Server runs with http://localhost:8989")
-
 	http.HandleFunc("/", htmlHandler)
-	http.HandleFunc("/get", getHandler)
-	http.HandleFunc("/postMessage", postHandler)
-	http.HandleFunc("/events", manager.SubscriptionHandler)
 
 	http.Handle("/res/", http.StripPrefix("/res", http.FileServer(http.Dir("templates/resource"))))
 
+	/*
+			conns = make(map[string]*Connection)
+		http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
+			s := websocket.Server{Handler: websocket.Handler(wsHandler)}
+			s.ServeHTTP(w, req)
+		})
+		if err := http.ListenAndServe(SERVE_WS, nil); err != nil {
+			panic("ListenAndServe: " + err.Error())
+		}
+	*/
+
+	conns = make(map[string]*Connection)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
+		s := websocket.Server{Handler: websocket.Handler(wsHandler)}
+		s.ServeHTTP(w, req)
+	})
+
 	// サーバーを起動
 	// http://localhost:8989
+	fmt.Println("The Server runs with http://localhost:8989")
 	http.ListenAndServe(":8989", nil)
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	// 情報取得API
-	responseJSON, _ := json.Marshal(messageDatabase)
-	fmt.Fprintln(w, string(responseJSON))
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	// 投稿API
-	var _ = r.ParseForm()
-	var update bool = false
-
-	for k := range r.Form {
-		messageDatabase.Message = append(messageDatabase.Message, createMessage(k))
-		update = true
-
-		if len(messageDatabase.Message) > 50 {
-			// メッセージ件数が上限を超える場合は古いデータから消す
-			unset := func(s []message, i int) []message {
-				if i >= len(s) {
-					return s
-				}
-				return append(s[:i], s[i+1:]...)
-			}
-
-			messageDatabase.Message = unset(messageDatabase.Message, 0)
-		}
-	}
-
-	if update {
-		manager.Publish("update", "message updated")
-	}
-}
-
-func createMessage(m string) message {
-	t := time.Now()
-	const timelayout = "15:04:05"
-	msg := message{Body: m, Time: t.Format(timelayout)}
-	return msg
 }
 
 func htmlHandler(w http.ResponseWriter, r *http.Request) {
@@ -102,5 +66,128 @@ func htmlHandler(w http.ResponseWriter, r *http.Request) {
 	// テンプレートを描画
 	if err := t.ExecuteTemplate(w, "template.html", dat); err != nil {
 		log.Fatal(err)
+	}
+}
+
+type Connection struct {
+	Channel     string
+	Conn        *websocket.Conn
+	Room        string
+	sendChannel chan Message
+}
+
+func NewConnection(ws *websocket.Conn) *Connection {
+	var id, _ = uuid.NewV4()
+	channel := id.String()
+	connection := &Connection{
+		Channel:     channel,
+		Conn:        ws,
+		sendChannel: make(chan Message)}
+	go channelHandler(connection)
+	msg := Message{
+		Type: TYPE_CONNECTED,
+		Msg:  channel}
+	connection.SendMessage(msg, connection)
+	return connection
+}
+
+func channelHandler(self *Connection) {
+	for msg := range self.sendChannel {
+		fmt.Printf("Sending %s channel:%s\n", msg.Type, self.Channel)
+		websocket.JSON.Send(self.Conn, msg)
+	}
+}
+
+func (self *Connection) EnterRoom(roomname string) {
+	self.Room = roomname
+}
+
+func (self *Connection) LeaveRoom(roomname string) {
+	self.Room = ""
+}
+func (self *Connection) IsJoiningRoom(targetRoom string) bool {
+	return self.Room == targetRoom
+}
+
+func (self *Connection) SendMessage(msg Message, from *Connection) {
+	msg.From = from.Channel
+	self.sendChannel <- msg
+}
+
+type Message struct {
+	Type string
+	Msg  string
+	From string
+}
+
+const (
+	DestTypeUnicast   = "uni"
+	DestTypeBroadcast = "bro"
+	DestTypeRoom      = "room"
+)
+
+type MessageFrame struct {
+	Type    string
+	Dest    string
+	Message Message
+}
+
+var conns map[string]*Connection
+
+func wsHandler(ws *websocket.Conn) {
+	conn := NewConnection(ws)
+	conns[conn.Channel] = conn
+	wsMsgHandler(conn)
+}
+
+func wsMsgHandler(conn *Connection) {
+	for {
+		var frame MessageFrame
+		err := websocket.JSON.Receive(conn.Conn, &frame)
+		if err != nil {
+			fmt.Println(err)
+			onDisconnected(conn)
+			return
+		}
+
+		fmt.Printf("Received: %s from[%s]\n", frame.Type, conn.Channel)
+
+		if frame.Message.Type == TYPE_ENTER {
+			onEnter(conn, frame)
+		} else {
+			frame.Message.From = conn.Channel
+			switch frame.Type {
+			case DestTypeUnicast:
+				conns[frame.Dest].SendMessage(frame.Message, conn)
+			default:
+				broadcastRoom(frame.Message, frame.Dest, conn)
+			}
+		}
+	}
+}
+
+func onEnter(conn *Connection, frame MessageFrame) {
+	fmt.Printf("channel[%s] entered room[%s]\n", conn.Channel, frame.Dest)
+	conn.EnterRoom(frame.Dest)
+	broadcastRoom(frame.Message, frame.Dest, conn)
+}
+
+func onDisconnected(conn *Connection) {
+	fmt.Printf("channel[%s] dicconnected\n", conn.Channel)
+	delete(conns, conn.Channel)
+	msg := Message{
+		Type: TYPE_LEAVE,
+		Msg:  conn.Channel,
+		From: conn.Channel,
+	}
+	broadcastRoom(msg, conn.Room, conn)
+}
+
+func broadcastRoom(msg Message, room string, from *Connection) {
+	fmt.Printf("------ broadcast room:[%s] from:[%s] type[%s] --------\n", room, from.Channel, msg.Type)
+	for _, conn := range conns {
+		if conn.IsJoiningRoom(room) {
+			conn.SendMessage(msg, from)
+		}
 	}
 }
---
+++ 07-voicechat/templates/resource/js/index.js
@@ -0,0 +1,316 @@
+// web rtc test
+(function (global) {
+
+  var localVideo = document.getElementById('local-video');
+  var remoteVideo = document.getElementById('remote-video');
+  var localStream = null;
+  var peerConnection = null;
+  var peerStarted = false;
+  var mediaConstraints = {
+    'mandatory': {
+      'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true
+    }
+  };
+  // ----------------- handshake --------------
+  var textForSendSDP = document.getElementById('text-for-send-sdp');
+  var textForSendICE = document.getElementById('text-for-send-ice');
+  var iceSeparator = '------ ICE Candidate -------';
+  var CR = String.fromCharCode(13);
+  var myChannel = null;
+
+  var connections = {}; // key: channel, value: connection
+
+  // initialize websocket
+  var url = "ws://" + location.host + "/ws";
+  var ws = new Socket(url, function (type, msg) {
+
+    console.log('received type:' + type)
+
+    if (type === 'enter') {
+      sendOffer(msg);
+    } else if (type === 'offer') {
+      onOffer(msg);
+    } else if (type === 'answer') {
+      onAnswer(msg);
+    } else if (type === 'candidate') {
+      onCandidate(msg);
+    } else if (type === 'user dissconnected') {
+      stop();
+    } else if (type === 'leave') {
+      onLeave(msg);
+    }
+  });
+
+  // logic
+
+  // -- start flow --
+  // start local video
+
+  // ---------------------- media handling ----------------------- 
+  // start local video
+  function startVideo() {
+
+    return new Promise(function (resolve, reject) {
+      getDeviceStream({ video: true, audio: false })
+        .then(function (stream) { // success
+          console.log('getUserMedia success')
+          localStream = stream;
+          playVideo(localVideo, stream);
+          console.log('localStream ready')
+          resolve();
+        }).catch(function (error) { // error
+          console.error('getUserMedia error:', error);
+          return;
+        });
+    });
+  }
+
+  // stop local video
+  function stopVideo() {
+    pauseVideo(localVideo);
+    stopLocalStream(localStream);
+  }
+
+  function stopLocalStream(stream) {
+    let tracks = stream.getTracks();
+    if (!tracks) {
+      console.warn('NO tracks');
+      return;
+    }
+
+    for (let track of tracks) {
+      track.stop();
+    }
+  }
+
+  function getDeviceStream(option) {
+
+    if ('getUserMedia' in navigator.mediaDevices) {
+      console.log('navigator.mediaDevices.getUserMadia');
+      return navigator.mediaDevices.getUserMedia(option);
+    }
+    else {
+      console.log('wrap navigator.getUserMadia with Promise');
+      return new Promise(function (resolve, reject) {
+        navigator.getUserMedia(option,
+          resolve,
+          reject
+        );
+      });
+    }
+  }
+
+  function playVideo(element, stream) {
+    if ('srcObject' in element) {
+      element.srcObject = stream;
+    }
+    else {
+      element.src = window.URL.createObjectURL(stream);
+    }
+    element.play();
+    element.volume = 0;
+  }
+
+  function pauseVideo(element) {
+    element.pause();
+    if ('srcObject' in element) {
+      element.srcObject = null;
+    }
+    else {
+      if (element.src && (element.src !== '')) {
+        window.URL.revokeObjectURL(element.src);
+      }
+      element.src = '';
+    }
+  }
+
+
+  // pork to all
+  function enterRoom() {
+    var roomname = getRoomName();
+    ws.enter(roomname);
+  }
+
+  // start the connection upon user request
+  function sendOffer(data) {
+    var conn = prepareNewConnection(data.From);
+    conn.createOffer(function (sessionDescription) {
+      // in case of success
+      conn.peer.setLocalDescription(sessionDescription);
+      sendSDP(data.From, sessionDescription);
+    }, function () {
+      // in case of error
+      console.log("Create Offer failed");
+    }, mediaConstraints);
+  }
+
+  // -- on offer received flow --
+
+  function onOffer(msg) {
+    var sdp = JSON.parse(msg.Msg);
+    var connection = prepareNewConnection(msg.From);
+    connection.peer.setRemoteDescription(new RTCSessionDescription(sdp));
+    addConnection(connection);
+    sendAnswer(msg);
+  }
+
+  function sendAnswer(evt) {
+    var conn = getConnection(evt.From)
+    var peer = conn.peer;
+    peer.createAnswer(function (sessionDescription) { // in case of success
+      peer.setLocalDescription(sessionDescription);
+      sendSDP(evt.From, sessionDescription);
+    }, function () { // in case of error
+      console.log("Create Answer failed");
+    }, mediaConstraints);
+  }
+
+  // ---- on answer ----
+  function onAnswer(evt) {
+    var data = eval("(" + evt.Msg + ')');
+
+    var conn = getConnection(evt.From);
+    conn.peer.setRemoteDescription(new RTCSessionDescription(data));
+  }
+  // handle disconnected
+
+  function onLeave(msg) {
+    console.log('remove connection:' + msg.From)
+    removeConnection(msg.From)
+    $('#' + msg.From).remove();
+  }
+
+  // ----  utility ----
+
+
+  // peer connection
+  function prepareNewConnection(id) {
+    var connection = new Connection(id);
+    addConnection(connection);
+
+    var pc_config = { "iceServers": [] };
+    var peer = null;
+
+    try {
+      peer = new RTCPeerConnection(pc_config);
+      // send any ice candidates to the other peer
+      peer.onicecandidate = function (evt) {
+        if (evt.candidate) {
+          console.log('on candidate');
+          sendCandidate(connection.id, {
+            type: "candidate",
+            sdpMLineIndex: evt.candidate.sdpMLineIndex,
+            sdpMid: evt.candidate.sdpMid,
+            candidate: evt.candidate.candidate
+          });
+        } else {
+          console.log("candidate end. ------- phase=" + evt.eventPhase);
+          connection.ready = true;
+        }
+      };
+
+      peer.addStream(localStream);
+
+      peer.addEventListener("addstream", function (event) {
+        console.log("Added remote stream");
+        var $video = $('<video class="remote-video" autoplay></video>');
+        $video.attr('id', id);
+        $("#remote-videos").append($video);
+        // $video.get(0).src = window.URL.createObjectURL(event.stream);
+        playVideo($video.get(0), event.stream);
+      }, false);
+
+      peer.addEventListener("removestream", function (event) {
+        // when remote removes a stream, remove it from the local video element
+        $('#' + id).remove();
+      }, false);
+
+      connection.peer = peer;
+
+    } catch (e) {
+      console.log("Failed to create peerConnection, exception: " + e.message);
+    }
+    return connection;
+  }
+
+  function sendCandidate(id, candidate) {
+    var text = JSON.stringify(candidate);
+    textForSendICE.value = (textForSendICE.value + CR + iceSeparator + CR + text + CR);
+    textForSendICE.scrollTop = textForSendICE.scrollHeight;
+    ws.unicast(id, candidate.type, candidate)
+  }
+
+  function sendSDP(id, sdp) {
+    var text = JSON.stringify(sdp);
+    textForSendSDP.value = text;
+    ws.unicast(id, sdp.type, sdp)
+  }
+
+  // stop the connection upon user request
+  function stop() {
+    peerConnection.close();
+    peerConnection = null;
+    peerStarted = false;
+  }
+
+  function onCandidate(msg) {
+    var evt = JSON.parse(msg.Msg);
+    var conn = getConnection(msg.From);
+
+    var candidate = new RTCIceCandidate({
+      sdpMLineIndex: evt.sdpMLineIndex,
+      sdpMid: evt.sdpMid,
+      candidate: evt.candidate
+    });
+    conn.peer.addIceCandidate(candidate);
+  }
+
+  function isLocalStreamReady() {
+    return localStream != null;
+  }
+
+  function getRoomName() {
+    var url = location.href;
+    urls = url.split("?");
+    if (1 < urls.length && urls[1]) {
+      return urls[1];
+    } else {
+      room = prompt('ルーム名を入力してください', 'general');
+      location.search = room;
+      return room;
+    }
+  }
+
+  function addConnection(conn) {
+    connections[conn.id] = conn;
+  }
+  function removeConnection(connId) {
+    delete connections[connId];
+  }
+
+  function getConnection(id) {
+    return connections[id];
+  }
+
+
+  // stop local video
+  function stopVideo() {
+    localVideo.src = "";
+    localStream.stop();
+    localStream = null;
+  }
+
+  function hangUp() {
+    console.log("Hang up.");
+    stop();
+  }
+
+  try {
+    startVideo().then(function () {
+      enterRoom();
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+
+})(window);
